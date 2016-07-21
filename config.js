5a31d5329e6eed21e73a1bcced455c2e31676c51
System Administrator <root@daves-mbp-2.home>
2016-07-21 14:18:01 +0200

    fix

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 80,
 	stormpath: {
-		id: process.env.STORMPATH_ID ? process.env.STORMPATH_ID : '310HWH2M6UPYINU7FO1484W0X',
-		secret: process.env.STORMPATH_SECRET ? process.env.STORMPATH_SECRET : 'SNaz2bTgaA/e2JCTvVXJjgg+22PDz+OR/+/W7Glt1rg',
-		appId: process.env.STORMPATH_APPID ? process.env.STORMPATH_APPID : '2dOFY5UnOtoCGznZKlA5ax',
+		id: process.env.STORMPATH_ID ? process.env.STORMPATH_ID : '',
+		secret: process.env.STORMPATH_SECRET ? process.env.STORMPATH_SECRET : '',
+		appId: process.env.STORMPATH_APPID ? process.env.STORMPATH_APPID : '',
 	},
 	sendgrid: {
 		username: process.env.SENDGRID_USERNAME ? process.env.SENDGRID_USERNAME : '',
