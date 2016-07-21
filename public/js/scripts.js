46cf26d75bf5830114c4c2b8f6a8400b7ed1a255
System Administrator <root@daves-mbp-2.home>
2016-07-21 13:10:29 +0200

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,61 +1,62 @@
-	/* http://tedxchatbot.herokuapp.com  */
-	var socket = io.connect('http://tedxchatbot.herokuapp.com');
+var SOCKET_SERVER_URL = 'http://tedxchatbot.herokuapp.com';
 
-	socket.on('connect', function() {
-		console.log('Client has connected to the server!');
-		socket.on('message', function(data) {
-			console.log(data);
-			$('#content').delay(textDelay).queue(function(n) {
-				if (data.text && data.text.search('payload') == -1) {
-					printChat(data, false);
-				} else {
-					data.text = JSON.parse(data.text);
+function messageReducer(state, action) {
+  if (typeof state === 'undefined') {
+    return {body: null};
+  }
+  switch (action.type) {
+    case 'MESSAGE_RECEIVED':
+      return {body: action.message};
+    default:
+      return {body: null};
+  }
+}
 
-					if ('image' == data.text.attachment.type) {
-						printChat(data, false); //  data.text.attachment.payload.url
-					} else if ('login' == data.text.attachment.type) {
-						User.signin(data.text.attachment.payload.username, data.text.attachment.payload.password, {
-							cache: true
-						}).done(function(user) {
-							console.log('Logged in', user.data);
+function userReducer(state, action) {
+	if (typeof state === 'undefined') {
+    return {};
+  }
+	switch(action.type) {
+		case 'LOGGEDIN':
+			return {user: action.user};
+		default:
+			return {user: null};
+	}
+}
+function attachmentReducer(state, action) {
+	if (typeof state === 'undefined') {
+    return {};
+  }
+	switch(action.type) {
+		case 'ATTACHMENT_RECEIVED':
+			if('login' == action.attachment.type) {
+				return {login: action.attachment.payload};
+			}
+			else if('image' == action.attachment.type){
+				return {image: null};
+			}
+		default:
+			return {};
+	}
+}
+var rootReducer = Redux.combineReducers({
+    message: messageReducer,
+		user: userReducer,
+		attachment: attachmentReducer
+});
 
-							var data = {
-								user: 'tedxbot',
-								text: 'Master ' + user.data.firstname + ' you are now logged in !'
-							};
+var store = Redux.createStore(rootReducer);
 
-							printChat(data, false);
+function render() {
+	state = store.getState();
+	console.log('state', state);
 
-						}).fail(function(err) {
-							console.log(err);
-
-							var data = {
-								user: 'tedxbot',
-								text: 'Your password appears to be incorrect'
-							};
-							printChat(data, false);
-						});
-					}
-				}
-				n();
-			});
-		});
-	});
-
-	socket.on('disconnect', function() {
-		console.log('The client has disconnected!');
-	});
-
-	function printChat(data, me) {
+	// Print message from bot.
+	if(state.message.body) {
 		var who = 'them';
-		if (me) {
-			who = 'me';
-		} else {
-			data.avatar = '//pi.tedcdn.com/r/pe.tedcdn.com/images/ted/c9928d59974a7d5b8f8889794634cbded07ff266_1600x1200.jpg?c=1050%2C550&w=180';
-		}
+		var avatar = '/images/avatar.png';
 		$('#content').delay(textDelay).queue(function(n) {
-			console.log('print', data);
-			var templ = '<div class="message-wrapper ' + who + '"><div class="circle-wrapper animated bounceIn" style="background-image:url('+data.avatar+'); background-position:-70px -285px"></div><div class="text-wrapper animated fadeIn">' + data.text + '</div></div>';
+			var templ = '<div class="message-wrapper ' + who + '"><div class="circle-wrapper animated bounceIn" style="background-image:url('+avatar+'); background-size: 50px 50px;background-position:-5px -5px"></div><div class="text-wrapper animated fadeIn">' + state.message.body + '</div></div>';
 			$(this).append(templ);
 			$('#input').val('');
 			scrollBottom();
@@ -63,95 +64,166 @@
 		});
 	}
 
-	function sendChat(message, me) {
-		var text = message;
-		if (!message) {
-			text = $('#input').val()
-		}
-		console.log('sending: ' + text);
-		var data = {
-			user: 'You',
-			channel: socket.io.engine.id,
-			text: text,
-			data: currentUser
-		};
-		socket.emit('message', data);
-		if (!message) {
-			printChat(data, me);
-		}
-	}
-
-	$.fn.enterKey = function(fnc) {
-		return this.each(function() {
-			$(this).keypress(function(ev) {
-				var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-				if (keycode == '13') {
-					fnc.call(this, ev);
-				}
-			});
+	// Login user.
+	if(state.attachment.login) {
+		User.signin(state.attachment.login.username, state.attachment.login.password, {
+			cache: true
+		}).done(function(user) {
+			store.dispatch(loggedInAction(user.data));
+			store.dispatch(messageReceivedAction('Master ' + user.data.firstname + ' you are now logged in !'));
+		}).fail(function(err) {
+			console.log(err);
+			store.dispatch(messageReceivedAction('Your password appears to be incorrect'));
 		});
 	}
+}
 
-	$(function() {
+function loggedInAction(userData) {
+	return {
+		type: 'LOGGEDIN',
+		user: userData
+	}
+}
+
+function messageReceivedAction(message) {
+	return {
+		type: 'MESSAGE_RECEIVED',
+		message: message
+	}
+}
+
+function attachmentReceivedAction(attachment) {
+	return {
+		type: 'ATTACHMENT_RECEIVED',
+		attachment: attachment
+	}
+}
+
+render()
+store.subscribe(render);
+
+var socket = io.connect(SOCKET_SERVER_URL);
+socket.on('connect', function() {
+	console.log('Client has connected to the server!');
+	socket.on('message', function(data) {
+		//console.log(data);
 		$('#content').delay(textDelay).queue(function(n) {
-			var data = {
-				user: 'tedxbot',
-				text: 'Hello!'
-			};
-			if (currentUser && currentUser.data && currentUser.data.email) {
-				data.text = 'Welcome back ' + currentUser.data.firstname;
+			if (data.text && data.text.search('payload') == -1) {
+				store.dispatch(messageReceivedAction(data.text));
+			} else {
+				data.text = JSON.parse(data.text);
+				if(data.text.attachment.type) {
+					store.dispatch(attachmentReceivedAction(data.text.attachment));
+				}
 			}
-			printChat(data, false);
 			n();
 		});
+	});
+});
 
-		$("form").on('submit', function(e) {
-			e.preventDefault();
-		});
+socket.on('disconnect', function() {
+	console.log('The client has disconnected!');
+});
 
-		$("#input").enterKey(function() {
-			sendChat(false, true);
-		})
+function printChat(text) {
+	who = 'me';
+	$('#content').delay(textDelay).queue(function(n) {
+		var templ = '<div class="message-wrapper ' + who + '"><div class="animated bounceIn"></div><div class="text-wrapper animated fadeIn">' + text + '</div></div>';
+		$(this).append(templ);
+		$('#input').val('');
+		scrollBottom();
+		n();
+	});
+}
 
-		$('#send').on('click', function() {
-			sendChat(false, true);
-		});
+function sendChat(message, me) {
+	var text = message;
+	if (!message) {
+		text = $('#input').val()
+	}
+	console.log('sending: ' + text);
+	var data = {
+		user: 'You',
+		channel: socket.io.engine.id,
+		text: text,
+		data: currentUser
+	};
+	socket.emit('message', data);
+	if (!message) {
+		printChat(data.text);
+	}
+}
 
-		$('#login').on('click', function() {
-			login();
+$.fn.enterKey = function(fnc) {
+	return this.each(function() {
+		$(this).keypress(function(ev) {
+			var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+			if (keycode == '13') {
+				fnc.call(this, ev);
+			}
 		});
 	});
+}
 
-	function onLogin(user) {
-		// Do stuff upon user login.
-		console.log('User loggedin', user);
-		alert(user.email + ' is loggedin');
-	}
-
-	function login() {
+$(function() {
+	$('#content').delay(textDelay).queue(function(n) {
+		var data = {
+			text: 'Hello!'
+		};
 		if (currentUser && currentUser.data && currentUser.data.email) {
-			var user = currentUser;
-			onLogin(user.data);
-		} else {
-			var userObj = {
-				email: $('.signin-form #email').val(),
-				password: $('.signin-form #password').val(),
-			};
-			User.signin(userObj.email, userObj.password, {
-				cache: true
-			}).done(function(user) {
-				onLogin(user.data);
-			}).fail(function(err) {
-				console.log(err);
-			});
+			data.text = 'Welcome back ' + currentUser.data.firstname;
 		}
-	}
+		store.dispatch(messageReceivedAction(data.text));
+		n();
+	});
 
-	function scrollBottom() {
-			var $content = $('#content');
-		  var $inner = $('#inner');
-      $($inner).animate({ scrollTop: $($content).height() }, {
-          queue: true,
-          duration: 'ease'
-      });
-    }
+	$("form").on('submit', function(e) {
+		e.preventDefault();
+	});
+
+	$("#input").enterKey(function() {
+		sendChat(false, true);
+	})
+
+	$('#send').on('click', function() {
+		sendChat(false, true);
+	});
+
+	$('#login').on('click', function() {
+		login();
+	});
+});
+
+function onLogin(user) {
+	// Do stuff upon user login.
+	console.log('User loggedin', user);
+	alert(user.email + ' is loggedin');
+}
+
+function login() {
+	if (currentUser && currentUser.data && currentUser.data.email) {
+		var user = currentUser;
+		onLogin(user.data);
+	} else {
+		var userObj = {
+			email: $('.signin-form #email').val(),
+			password: $('.signin-form #password').val(),
+		};
+		User.signin(userObj.email, userObj.password, {
+			cache: true
+		}).done(function(user) {
+			onLogin(user.data);
+		}).fail(function(err) {
+			console.log(err);
+		});
+	}
+}
+
+function scrollBottom() {
+	var $content = $('#content');
+	var $inner = $('#inner');
+  $($inner).animate({ scrollTop: $($content).height() }, {
+  	queue: true,
+    duration: 'ease'
+  });
+}
